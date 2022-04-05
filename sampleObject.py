ee0c4d265166153bfc3d7f7caaca9b472e4d7953
Waleed Allaudin <87147415+wal238@users.noreply.github.com>
2022-04-05 18:41:45 -0400

    Add files via upload

---
+++ sampleObject.py
@@ -0,0 +1,8 @@
+class Person:
+
+    members = []
+
+    def __init__(self, name, email, paymentStreak):
+        self.name = name
+        self.email = email
+        self.paymentStreak = paymentStreak
