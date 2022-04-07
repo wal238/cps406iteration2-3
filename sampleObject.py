c135d570c1808cf01b8fa9566ca5f294397bb10f
Waleed <allaudindeveloper@gmail.com>
2022-04-07 17:12:51 -0400

    Added payment_method

--- sampleObject.py
+++
@@ -1,8 +0,0 @@
-class Person:
-
-    members = []
-
-    def __init__(self, name, email, paymentStreak):
-        self.name = name
-        self.email = email
-        self.paymentStreak = paymentStreak
